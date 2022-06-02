800d17e2f49a4aef15e7e76458f19bef02f2d6a3
Noam Ross <ross@ecohealthalliance.org>
2022-06-01 21:55:00 -0400

    Add renv instructions in run.R comments
    Closes #1

--- run.R
+++ run.R
@@ -5,6 +5,13 @@
 # See https://books.ropensci.org/targets/hpc.html
 # to learn about your options.
 
+# Prior to running this script, run `renv::restore()`
+# (running `install.packages('renv')` if neccessary first).
+# This will install all the packages listed in `renv.lock`
+# needed to run this analysis.  cmdstan may also need to be
+# installed afterwords, which can be done with
+# `cmdstanr::install_cmdstan(version = "2.29.2")`
+
 targets::tar_make()
 # targets::tar_make_clustermq(workers = 2) # nolint
 # targets::tar_make_future(workers = 2) # nolint
